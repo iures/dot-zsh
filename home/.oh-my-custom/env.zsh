d60a9ad739a3dc8f4b3db1f4f78ccc5139d260d6
Iure Sales <iureguimaraes@gmail.com>
2018-02-12 13:33:40 -0500

    Remove brew

--- home/.oh-my-custom/env.zsh
+++ home/.oh-my-custom/env.zsh
@@ -3,6 +3,3 @@ export KUBECONFIG="$HOME/.kube/production-config"
 setopt interactivecomments
 
 source "$HOME/.vim/bundle/gruvbox/gruvbox_256palette_osx.sh"
-
-export PATH="$(brew --prefix qt@5.5)/bin:$PATH"
-
